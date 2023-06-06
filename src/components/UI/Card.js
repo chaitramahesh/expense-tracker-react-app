52e2d373f6357f2b94b8ed915f8ad4317c0f8fa8
chaitramahesh <131942541+chaitramahesh@users.noreply.github.com>
2023-06-06 22:50:19 +0530

    Update Card.js

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -11,8 +11,5 @@ function Card(props) {
 
 export default Card;
 
-// to use components as wrapper element  like <div>
-//(i.e as opening and closing tags ex: as in Expenses and ExpenseItem)
-// we have to set children and classNames etc. bcz it does not support everything by 
-// default as <div> does. it supports only those which we well ask them to support
+
 
